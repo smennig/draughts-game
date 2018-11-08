dd1b5aadc4b22785cee5857d4d2bf5633a36a318
Tim <tim.hardenacke@hiqs.de>
2018-11-08 12:26:57 +0100

    Added ScalaFx Support

--- build.sbt
+++ build.sbt
@@ -6,3 +6,6 @@ scalaVersion := "2.12.7"
 
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+libraryDependencies += "org.scalafx" %% "scalafx" % "8.0.144-R12"
+
+mainClass in (Compile, run) := Some("Main")
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,59 @@
+package main
+
+import view.BeginGameGUI
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color._
+import scalafx.scene.paint._
+import scalafx.scene.text.Text
+
+
+object Main extends JFXApp {
+  /*
+    stage = new JFXApp.PrimaryStage {
+      title.value = "Neues Spiel"
+      width = 600
+      height = 450
+      scene = new BeginGameGUI().getStartGameScene()
+    }
+    */
+  stage = new PrimaryStage {
+    //    initStyle(StageStyle.Unified)
+    title = "ScalaFX Hello World"
+    scene = new Scene {
+      fill = Color.rgb(38, 38, 38)
+      content = new HBox {
+        padding = Insets(50, 80, 50, 80)
+        children = Seq(
+          new Text {
+            text = "Scala"
+            style = "-fx-font: normal bold 100pt sans-serif"
+            fill = new LinearGradient(
+              endX = 0,
+              stops = Stops(Red, DarkRed))
+          },
+          new Text {
+            text = "FX"
+            style = "-fx-font: italic bold 100pt sans-serif"
+            fill = new LinearGradient(
+              endX = 0,
+              stops = Stops(White, DarkGray)
+            )
+            effect = new DropShadow {
+              color = DarkGray
+              radius = 15
+              spread = 0.25
+            }
+          }
+        )
+      }
+    }
+
+  }
+}
