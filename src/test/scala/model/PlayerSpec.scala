2e1add04fdddac89e16d7a365a7b6bbb6e3c263b
Tim <tim.hardenacke@hiqs.de>
2018-12-13 11:40:32 +0100

    Fixed unpulled merge

--- src/test/scala/model/PlayerSpec.scala
+++ src/test/scala/model/PlayerSpec.scala
@@ -2,7 +2,6 @@ package model
 
 import org.scalatest.Matchers._
 import org.scalatest.WordSpec
-import java.io.ByteArrayInputStream
 import controller.CommandLineController
 
 class PlayerSpec extends WordSpec {
@@ -23,11 +22,34 @@ class PlayerSpec extends WordSpec {
     "created" should {
       val controller = new CommandLineController()
       "be able to choose the color black" in {
-        val in = new ByteArrayInputStream("Schwarz".getBytes)
-        System.setIn(in)
-        val color = controller.chooseFirstColor()
+        val color = controller.chooseFirstColor("Schwarz")
+        color should be (Option(Colour.BLACK))
+      }
+      "be able to choose the color white" in {
+        val color = controller.chooseFirstColor("Weiß")
+        color should be (Option(Colour.WHITE))
+      }
+      "not allow invalid string inputs" in {
+        val color = controller.chooseFirstColor("asdasdasfrgdf")
+        color should be (None)
+      }
+      "choose the color white if the first player is black" in {
+        val color = controller.chooseSecondColor(Colour.BLACK)
+        color should be (Colour.WHITE)
+      }
+      "choose the color black if the first player is white" in {
+        val color = controller.chooseSecondColor(Colour.WHITE)
         color should be (Colour.BLACK)
+      }
+      "print the translated color for black" in {
+        val translatedBlackColor = controller.translateEnumColour(Colour.BLACK)
+        translatedBlackColor should be ("Schwarz")
+      }
+      "print the translated color for white" in {
+        val translatedBlackColor = controller.translateEnumColour(Colour.WHITE)
+        translatedBlackColor should be ("Weiß")
       }
     }
   }
 }
+
